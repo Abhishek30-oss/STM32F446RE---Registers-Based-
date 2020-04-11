85ff4860276a1f89a7efee2b4c90b68db5cce8d9
Abhishek30-oss <62514105+Abhishek30-oss@users.noreply.github.com>
2020-04-11 14:04:43 +0530

    GPIO EXTI

--- 001_External Interrupt/main.c
+++ 001_External Interrupt/main.c
@@ -21,33 +21,33 @@ void EXTI15_10_IRQHandler(void)
 	   EXTI->PR |=(1<<13);									//Clear Pending Register
 	 }		 
 	 
-	GPIOA->ODR = ~(GPIOA->ODR) ;						//LED Toggle
+		GPIOA->ODR = ~(GPIOA->ODR) ;						//LED Toggle
 }
 
 int main()
 {
 	
-		RCC->AHB1ENR |= ((1<<0) | (1<<2));		//COnfigure clock for PORT A and PORT C
+				RCC->AHB1ENR |= ((1<<0) | (1<<2));		//COnfigure clock for PORT A and PORT C
 	
 		//Configuring  PA5 as OUTPUT
-      GPIOA->MODER &= ~(1<<11);	
-      GPIOA->MODER |= (1<<10);
+				GPIOA->MODER &= ~(1<<11);	
+				GPIOA->MODER |= (1<<10);
 	
 	//IN STM32F446RE to use line as interrupt configure it in input mode.
 	//Configuring  PC5 as INPUT
-	    GPIOC->MODER  &= ~(1<<26) ;
-	    GPIOC->MODER &= ~(1<<27);
+				GPIOC->MODER  &= ~(1<<26) ;
+				GPIOC->MODER &= ~(1<<27);
 	
 	//Configure EXTI Line
-			RCC->APB2ENR 			|= 0x00004000;
-			SYSCFG->EXTICR[3]	 = 0x00000020;
+				RCC->APB2ENR 			|= 0x00004000;
+				SYSCFG->EXTICR[3]	 = 0x00000020;
   
 	 
-			 EXTI->FTSR |= (1<<13);			//Falling Edge
-       EXTI->IMR  |= (1<<13);      //Unmask PC13 -set 1
+				EXTI->FTSR |= (1<<13);			//Falling Edge
+				EXTI->IMR  |= (1<<13);      //Unmask PC13 -set 1
 	    
-	     NVIC_SetPriority(EXTI15_10_IRQn,0);
-	     NVIC_EnableIRQ( EXTI15_10_IRQn);
+				NVIC_SetPriority(EXTI15_10_IRQn,0);
+				NVIC_EnableIRQ( EXTI15_10_IRQn);
       
       while(1);
 			
